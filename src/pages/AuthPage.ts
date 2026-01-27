ca565c08a8f14ab4f8a64008ba5ab4cd21e924e9
MdTajwarAliRumman <tajwaralirumman@gmail.com>
2026-01-27 16:18:05 +0600

    Register Flow Positive TestCase

--- src/pages/AuthPage.ts
+++ src/pages/AuthPage.ts
@@ -16,6 +16,28 @@ export class AuthPage {
 
     readonly AccountInfoTitle: Locator;
 
+    readonly AccountNameTextBox: Locator;
+    readonly PasswordTextBox: Locator;
+    readonly DayDropDown: Locator;
+    readonly MonthDropDown: Locator;
+    readonly YearDropDown: Locator;
+    readonly NewsSettlerCheckbox: Locator;
+    readonly OffersCheckbox: Locator;
+    readonly firstName: Locator;
+    readonly lastName: Locator;
+    readonly company: Locator;
+    readonly address: Locator;
+    readonly address2: Locator;
+    readonly country: Locator;
+    readonly state: Locator;
+    readonly city: Locator;
+    readonly zipCode: Locator;
+    readonly mobileNumber: Locator;
+    readonly createAccountButton: Locator;
+    readonly continueButton: Locator;
+    readonly loggedInUser: Locator;
+    readonly userDeleteBtn: Locator;
+
     constructor(page: Page) {
         this.page = page
 
@@ -29,6 +51,28 @@ export class AuthPage {
         this.LoginButton = page.locator("//button[normalize-space()='Login']")
 
         this.AccountInfoTitle = page.getByText('ENTER ACCOUNT INFORMATION')
+
+        this.AccountNameTextBox = page.locator("//input[@id='name']")
+        this.PasswordTextBox = page.locator("//input[@id='password']")
+        this.DayDropDown = page.locator("//select[@id='days']")
+        this.MonthDropDown = page.locator("//select[@id='months']")
+        this.YearDropDown = page.locator("//select[@id='years']")
+        this.NewsSettlerCheckbox = page.locator("//input[@id='newsletter']")
+        this.OffersCheckbox = page.locator("//input[@id='optin']")
+        this.firstName = page.locator("//input[@id='first_name']")
+        this.lastName = page.locator("//input[@id='last_name']")
+        this.company = page.locator("//input[@id='company']")
+        this.address = page.locator("//input[@id='address1']")
+        this.address2 = page.locator("//input[@id='address2']")
+        this.country = page.locator("//select[@id='country']")
+        this.state = page.locator("//input[@id='state']")
+        this.city = page.locator("//input[@id='city']")
+        this.zipCode = page.locator("//input[@id='zipcode']")
+        this.mobileNumber = page.locator("//input[@id='mobile_number']")
+        this.createAccountButton = page.locator("//button[normalize-space()='Create Account']")
+        this.continueButton = page.locator("//a[normalize-space()='Continue']")
+        this.loggedInUser = page.locator("//header[@id='header']//li[1]//a[1]")
+        this.userDeleteBtn = page.locator("//a[normalize-space()='Delete Account']")
     }
 
     // Methods
@@ -40,7 +84,28 @@ export class AuthPage {
         await this.NameTextBox.fill(name);
         await this.SignupEmailTextBox.fill(email);
         await this.SignupButton.click();
+    }
 
+    async AccountInfo(name: string, password: string, day: string, month: string, year: string, firstName: string, lastName: string, company: string,
+        address: string, address2: string, country: string, state: string, city: string, zipCode: string, mobileNumber: string) {
+        await this.AccountNameTextBox.fill(name);
+        await this.PasswordTextBox.fill(password);
+        await this.DayDropDown.selectOption(day);
+        await this.MonthDropDown.selectOption(month);
+        await this.YearDropDown.selectOption(year);
+        await this.NewsSettlerCheckbox.check();
+        await this.OffersCheckbox.check();
+        await this.firstName.fill(firstName);
+        await this.lastName.fill(lastName);
+        await this.company.fill(company);
+        await this.address.fill(address);
+        await this.address2.fill(address2);
+        await this.country.selectOption(country);
+        await this.state.fill(state);
+        await this.city.fill(city);
+        await this.zipCode.fill(zipCode);
+        await this.mobileNumber.fill(mobileNumber);
+        await this.createAccountButton.click();
     }
 
 }
--- tests/01_LoginRegister_Module/01_SignUp_test.spec.ts
+++ tests/01_LoginRegister_Module/01_SignUp_test.spec.ts
@@ -14,22 +14,47 @@ test.describe('Signup Flow', () => {
         homePage = new HomePage(page);
 
         await authPage.goToURL();
-        // await homePage.clickSignup();
+        await homePage.clickSignup();
     });
 
-    test('Verify that home page is visible successfully', async ({ page }) => {
-        await expect(page).toHaveTitle('Automation Exercise');
-    })
+    // this test is for homepage later on 
+    // test('Verify that home page is visible successfully', async ({ page }) => {
+    //     await expect(page).toHaveTitle('Automation Exercise');
+    // })
 
     test('Verify "New User Signup!" is visible', async ({ page }) => {
-        await homePage.clickSignup();
         await expect(page.getByText('New User Signup!')).toBeVisible();
     });
 
     test('Verify that "ENTER ACCOUNT INFORMATION" is visible', async ({ page }) => {
-        await homePage.clickSignup();
         await authPage.SignUp('Tajwar', generateEmail());
         await expect(page.getByText('ENTER ACCOUNT INFORMATION')).toBeVisible();
     });
+
+    test(" Verify that 'ACCOUNT CREATED!' is visible", async ({ page }) => {
+        await authPage.SignUp('Tajwar', generateEmail());
+        await authPage.AccountInfo('Md. Tajwar', '12345678', '10', 'March', '1995', 'Md. Tajwar', 'Ali', 'Softvence', '601/!, Mirpur',
+            'Dhaka', 'Australia', 'Dhaka', 'Dhaka', '1216', '01933954158');
+        await expect(page.getByText('Account Created!')).toBeVisible();
+    })
+
+    test(" Verify that 'logged in user' is visible", async ({ page }) => {
+        await authPage.SignUp('Tajwar', generateEmail());
+        await authPage.AccountInfo('Md. Tajwar', '12345678', '10', 'March', '1995', 'Md. Tajwar', 'Ali', 'Softvence', '601/!, Mirpur',
+            'Dhaka', 'Australia', 'Dhaka', 'Dhaka', '1216', '01933954158');
+        await authPage.continueButton.click();
+        await expect(authPage.loggedInUser).toBeVisible();
+
+    })
+
+    test(" Verify that 'ACCOUNT DELETED!' is visible", async ({ page }) => {
+        await authPage.SignUp('Tajwar', generateEmail());
+        await authPage.AccountInfo('Md. Tajwar', '12345678', '10', 'March', '1995', 'Md. Tajwar', 'Ali', 'Softvence', '601/!, Mirpur',
+            'Dhaka', 'Australia', 'Dhaka', 'Dhaka', '1216', '01933954158');
+        await authPage.continueButton.click();
+        await expect(authPage.loggedInUser).toBeVisible();
+        await authPage.userDeleteBtn.click();
+        await expect(page.getByText('ACCOUNT DELETED!')).toBeVisible();
+    })
 
 });
