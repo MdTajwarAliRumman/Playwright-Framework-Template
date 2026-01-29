8e98e1371c42f301969eaa1ce413407f6a1a4bf6
MdTajwarAliRumman <tajwaralirumman@gmail.com>
2026-01-29 17:33:00 +0600

    Contact popup message Test

--- src/pages/ContactPage.ts
+++ src/pages/ContactPage.ts
@@ -4,22 +4,49 @@ export class ContactPage {
     // initialize all the pages
 
     readonly page: Page;
-    readonly SignUpButton: Locator;
+    readonly ContactButton: Locator;
+    readonly NameTextBox: Locator;
+    readonly EmailTextBox: Locator;
+    readonly SubjectTextBox: Locator;
+    readonly MessageTextBox: Locator;
+    readonly SubmitButton: Locator;
+    readonly UploadFile: Locator;
 
     constructor(page: Page) {
         this.page = page
 
         // Elements
-        this.SignUpButton = page.locator("//a[normalize-space()='Signup / Login']")
+        this.ContactButton = page.locator("//a[normalize-space()='Contact us']")
+        this.NameTextBox = page.locator("//input[@placeholder='Name']")
+        this.EmailTextBox = page.locator("//input[@placeholder='Email']")
+        this.SubjectTextBox = page.locator("//input[@placeholder='Subject']")
+        this.MessageTextBox = page.locator("//textarea[@id='message']")
+        this.SubmitButton = page.locator("//input[@name='submit']")
+        this.UploadFile = page.locator("//input[@name='upload_file']")
     }
 
     // Methods
-    async goToURL() {
-        await this.page.goto(`${process.env.AUTOMATION_EXERCISES_URL}`);
+    async clickContact() {
+        await this.ContactButton.click();
     }
 
-    async clickSignup() {
-        await this.SignUpButton.click();
+    async contactFormDetails(name: string, email: string, subject: string, message: string) {
+
+        await this.NameTextBox.fill(name);
+        await this.EmailTextBox.fill(email);
+        await this.SubjectTextBox.fill(subject);
+        await this.MessageTextBox.fill(message);
+        // await this.UploadFile.setInputFiles('Images/SQA.jpg');
+        // await this.SubmitButton.click();
+    }
+
+    // This method is for alert message/popup message
+    async alertMessage() {
+        this.page.once('dialog', dialog => {
+            dialog.accept();
+            console.log(`Alert message is: ${dialog.message()}`);
+            console.log(`Dialog type is: ${dialog.type()}`);
+        })
     }
 }
 
--- tests/02_ContactUs_Module/TC06_ContactUs_Form_test.spec.ts
+++ tests/02_ContactUs_Module/TC06_ContactUs_Form_test.spec.ts
@@ -1,33 +1,38 @@
 import { test, expect } from '@playwright/test';
-import { AuthPage } from '../../src/pages/AuthPage';
+import { ContactPage } from '../../src/pages/ContactPage';
 import { HomePage } from '../../src/pages/HomePage';
 import dotenv from 'dotenv';
 dotenv.config();
 
-test.describe('Register Flow', () => {
-    let authPage: AuthPage;
+test.describe('Contact Flow', () => {
+    let contactPage: ContactPage;
     let homePage: HomePage;
 
     test.beforeEach(async ({ page }) => {
-        authPage = new AuthPage(page);
+        contactPage = new ContactPage(page);
         homePage = new HomePage(page);
 
         await homePage.goToURL();
-        await homePage.clickOnElement();
+        await homePage.clickOnElement(homePage.ContactButton);
     });
 
     // this test is for homepage later on 
     test('Verify that home page is visible successfully', async ({ page }) => {
-        await expect(page).toHaveTitle('Automation Exercise - Signup / Login');
+        await expect(page).toHaveTitle('Automation Exercise - Contact Us');
     })
 
-    test('Verify "New User Signup!" is visible', async ({ page }) => {
-        await expect(page.getByText('New User Signup!')).toBeVisible();
+    test('Verify "GET IN TOUCH" is visible', async ({ page }) => {
+        await expect(page.getByText('GET IN TOUCH')).toBeVisible();
     });
 
-    test("Verify error 'Email Address already exist!' is visible", async ({ page }) => {
-        await authPage.SignUp('Tajwar', process.env.USER_EMAIL!);
-        await expect(page.getByText('Email Address already exist!')).toBeVisible();
+    test("Verify success message 'Success! Your details have been submitted successfully.' is visible", async ({ page }) => {
+        await contactPage.contactFormDetails(`${process.env.USER_EMAIL}`, `${process.env.USER_EMAIL}`, `${process.env.DESCRIPTION_DEMO}`, `${process.env.DESCRIPTION_DEMO}`);
+        await contactPage.UploadFile.setInputFiles('Images/SQA.jpg');
+        await contactPage.SubmitButton.click();
+        // await contactPage.alertMessage();
+        await page.waitForTimeout(5000);
+        // await expect(page.getByText("Success! Your details have been submitted successfully.")).toBeVisible();
+
     });
 
 });
